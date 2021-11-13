967a3c275bb38ecacf2b3f1b1e859850a9024dfd
Pavliuk Maksym <pavliuk.maksym@chnu.edu.ua>
2021-11-13 16:12:36 +0200

    connected Entity Framework Core

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,20 +1,23 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
-using System;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Collections.Generic;
 
 namespace Server {
     public class Startup {
 
+        private IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+        
         public void ConfigureServices(IServiceCollection services) {
-
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<StackBurgerContext>(
+                options => options.UseSqlServer(connectionString)    
+            );
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
