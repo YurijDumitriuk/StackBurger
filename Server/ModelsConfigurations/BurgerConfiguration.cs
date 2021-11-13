36593b79d22d483b58b2fb365be7d4a7fafb1727
Pavliuk Maksym <pavliuk.maksym@chnu.edu.ua>
2021-11-13 16:03:47 +0200

    added models' configurations

---
+++ Server/ModelsConfigurations/BurgerConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Server.Models;
+
+namespace Server.ModelsConfigurations {
+    public class BurgerConfiguration : IEntityTypeConfiguration<Burger> {
+        public void Configure(EntityTypeBuilder<Burger> builder) {
+            builder.ToTable("burgers");
+            builder.Property(b => b.Id).HasColumnName("id");
+            builder.Property(b => b.IsCustom).HasColumnName("custom");
+            builder.Property(b => b.Name).HasColumnName("name");
+            builder.Property(b => b.Description).HasColumnName("description");
+            builder.HasKey(b => b.Id);
+        }
+    }
+}
---
+++ Server/ModelsConfigurations/OrderConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Server.Models;
+
+namespace Server.ModelsConfigurations {
+    public class OrderConfiguration : IEntityTypeConfiguration<Order> {
+        public void Configure(EntityTypeBuilder<Order> builder) {
+            builder.ToTable("orders");
+            builder.Property(o => o.Id).HasColumnName("id");
+            builder.Property(o => o.Date).HasColumnName("date");
+            builder.Property(o => o.UserId).HasColumnName("user_id");
+            builder.HasKey(o => o.Id);
+        }
+    }
+}
