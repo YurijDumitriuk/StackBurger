a6edd56810149d292ba03b00f5c0344bec53c05b
yclloud <dumitryuk.yuriy@chnu.edu.ua>
2021-11-22 15:28:35 +0200

    Create empty profile page and set redirecting for adding validation

--- Client/client/src/routes/Login.js
+++ Client/client/src/routes/Login.js
@@ -1,33 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 import './Login.css';
 import Navbar from "../components/Navbar";
-class Login extends Component{
-    render(){
-        return(
-            <div>
-                <Navbar />
-                <div className="FormWrapperLogin">
-                    <div className="LoginFormWrapper">
-                        <form className="LoginForm">
-                            <p className="LoginFormTitle">
-                                Welcome back
-                            </p>
-                            <p className="LoginFormField">
-                                <input type="tel" id="mobilePhone" placeholder="Mobile Phone (+380694201369)" required pattern="+[0-9]{12}"/>
-                            </p>
-                            <p className="LoginFormField">
-                                <input type="password" id="password" placeholder="Password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"/>
-                            </p>
-                            <p className="LoginFormButton">
-                                <button className="SubmitButton" id="submitbutton" type="button">ENTER</button>
-                            </p>
-                        </form>
-                    </div>
+import { useNavigate } from "react-router-dom";
+export default function Login() {
+    
+    const navigate = useNavigate();
+    function handleClick() {
+        /* тут до перехода можна вписати що треба зробити*/ 
+        navigate('/profile');
+    }
+
+    return(
+        <div>
+            <Navbar />
+            <div className="FormWrapperLogin">
+                <div className="LoginFormWrapper">
+                    <form className="LoginForm">
+                        <p className="LoginFormTitle">
+                            Welcome back
+                        </p>
+                        <p className="LoginFormField">
+                            <input type="tel" id="mobilePhone" placeholder="Mobile Phone (+380694201369)" required pattern="+[0-9]{12}"/>
+                        </p>
+                        <p className="LoginFormField">
+                            <input type="password" id="password" placeholder="Password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"/>
+                        </p>
+                        <p className="LoginFormButton">
+                            <button className="SubmitButton" onClick={()=>{handleClick()}} id="submitbutton" type="button">ENTER</button>
+                        </p>
+                    </form>
                 </div>
             </div>
-        );
-    }
-    
+        </div>
+    );
 }
-
-export default Login;
--- Client/client/src/routes/Registration.js
+++ Client/client/src/routes/Registration.js
@@ -1,38 +1,47 @@
-import React, { Component } from "react";
+import React from "react";
 import './Registration.css';
 import Navbar from "../components/Navbar";
-class Registration extends Component{
-    render(){
-        return(
-            <div>
-                <Navbar />
-                <div className="FormWrapper">
-                    <div className="RegistrationFormWrapper">
-                        <form className="RegistrationForm">
-                            <p className="RegistrationFormTitle">
-                                Create Account
-                            </p>
-                            <p className="RegistrationFormField">
-                                <input type="text" id="username" placeholder="Name" required/>
-                            </p>
-                            <p className="RegistrationFormField">
-                                <input type="tel" id="mobilePhone" placeholder="Mobile Phone (+380694201369)" required pattern="+[0-9]{12}"/>
-                            </p>
-                            <p className="RegistrationFormField">
-                                <input type="text" id="address" placeholder="Address" />
-                            </p>
-                            <p className="RegistrationFormField">
-                                <input type="password" id="password" placeholder="Password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"/>
-                            </p>
-                            <p className="RegistrationFormButton">
-                                <button className="SubmitButton" id="submitbutton" type="button">SIGN UP</button>
-                            </p>
-                        </form>
-                    </div>
+import {useNavigate } from "react-router-dom";
+    
+
+
+
+export default function Registration(){    
+    const navigate = useNavigate();
+    function handleClick() {
+        /* тут до перехода можна вписати що треба зробити*/ 
+        navigate('/login')
+    }
+    
+
+    return(
+        <div>
+            <Navbar />
+            <div className="FormWrapper">
+                <div className="RegistrationFormWrapper">
+                    <form className="RegistrationForm">
+                        <p className="RegistrationFormTitle">
+                            Create Account
+                        </p>
+                        <p className="RegistrationFormField">
+                            <input type="text" id="username" placeholder="Name" required/>
+                        </p>
+                        <p className="RegistrationFormField">
+                            <input type="tel" id="mobilePhone" placeholder="Mobile Phone (+380694201369)" required pattern="+[0-9]{12}"/>
+                        </p>
+                        <p className="RegistrationFormField">
+                            <input type="text" id="address" placeholder="Address" />
+                        </p>
+                        <p className="RegistrationFormField">
+                            <input type="password" id="password" placeholder="Password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"/>
+                        </p>
+                        <p className="RegistrationFormButton">
+                            <button className="SubmitButton" onClick={()=>{handleClick()}} id="submitbutton" type="button">SIGN UP</button>
+                        </p>
+                    </form>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
-export default Registration;
