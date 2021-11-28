1f6199e12a000a2ea4de9856f026bae5cd626d6b
Dima Golovaci <herous2001@gmail.com>
2021-11-28 14:12:01 +0200

    Fix Get Burgers

--- Server/Models/Burger/Burger.cs
+++ Server/Models/Burger/Burger.cs
@@ -7,7 +7,7 @@ namespace Server.Models {
         public bool IsCustom { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public Guid UserId { get; set; }
+        public Guid? UserId { get; set; }
 
         public List<Order> Orders { get; set; }
         public List<Component> Components { get; set; }
