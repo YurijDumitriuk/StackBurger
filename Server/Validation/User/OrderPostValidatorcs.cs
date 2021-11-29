7e497428d776b4e681b209fabe2b0187cc274532
Pavliuk Maksym <pavliuk.maksym@chnu.edu.ua>
2021-11-29 22:36:58 +0200

    implemented order post model validator

---
+++ Server/Validation/User/OrderPostValidatorcs.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Server.Models;
+
+namespace Server.Validation {
+    public class OrderPostValidator : AbstractValidator<OrderPostModel> {
+        public OrderPostValidator() {
+            RuleFor(b => b.Date)
+                .NotEmpty().WithMessage("Date is required!");
+            RuleFor(b => b.UserId)
+                .NotEmpty().WithMessage("User id is required!");
+            RuleFor(b => b.BurgersIds)
+                .NotNull().WithMessage("Burger ids are required!")
+                .NotEmpty().WithMessage("At least 1 burger!");
+        }
+    }
+}
