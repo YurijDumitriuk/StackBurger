e39dca7af1019e7d7fcde635f9751e83337d8ab3
Pavliuk Maksym <pavliuk.maksym@chnu.edu.ua>
2021-11-29 22:31:25 +0200

    implemented burger post model validator

---
+++ Server/Validation/BurgerPostValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Server.Models;
+
+namespace Server.Validation {
+    public class BurgerPostValidator: AbstractValidator<BurgerPostModel> {
+        public BurgerPostValidator() {
+            RuleFor(b => b.Name)
+                .NotEmpty().WithMessage("Name is required!");
+            RuleFor(b => b.UserId)
+                .NotEmpty().WithMessage("User id is required!");
+            RuleFor(b => b.ComponentsIds)
+                .NotNull().WithMessage("Components ids are required!")
+                .Must(ids => ids.Count >= 3).WithMessage("At least 3 components!");                          
+        }
+    }
+}
