004c01df1fba0013d9b0e259e6f5a93838ec9174
Dima Golovaci <herous2001@gmail.com>
2021-12-06 23:55:23 +0200

    Create post order method on client-side

--- Client/client/src/components/Order.js
+++ Client/client/src/components/Order.js
@@ -1,16 +1,43 @@
 import React from "react";
 import './Order.css';
 import OrderBurgerCard from "../components/OrderBurgerCard";
+import { PostOrder } from "../services/OrderService";
 
-let orderItemList=[]//Ось тобі масив куда піхати
-let totalPrice;
+let orderItemList=[];
+let totalPrice = 0;
 
+
+async function ConfirmOrder(){
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    //var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date.toString();//+' '+time;
+    var userId = localStorage.getItem("userId");
+    var burgers = JSON.parse(localStorage.getItem("burgersInCart"));
+    console.log("Order confirmed: ", dateTime);
+    var result = await PostOrder(dateTime, userId, burgers);
+    if(result === true){
+        console.log("Redirecting to history...")
+        window.location="/profile_history";
+    }
+}
+
+function Sort(a, b){
+    if (a.name > b.name) {
+    return 1;
+    }
+    if (a.name < b.name) {
+    return -1;
+    }
+    return 0;
+}
 
 function CreateCartList(){
     let burgers = [];
     let result = localStorage.getItem("burgersInCart");
     if(result !== null){
         burgers = JSON.parse(result);
+        burgers.sort(Sort);
         orderItemList = [];
         totalPrice = 0;
         burgers.forEach(b => {
@@ -31,9 +58,9 @@ export default function Order(){
                     {orderItemList}
                 </div>
 
-                <p className="PriceText">Total price : ${totalPrice}</p>
+                <p className="PriceText">Total price : ${totalPrice.toFixed(2)}</p>
                 <div className="SubmitOrderButtonWraper">
-                    <button className="SubmitOrderButton">Submit</button>
+                    <button className="SubmitOrderButton" onClick={() => ConfirmOrder()}>Submit</button>
                 </div>
             </div>
     );
